e4b84076e2bd63434750b9f8d231a4f01bcf2ba7
Koemsak Mean <koemsak.mean@student.passerellesnumeriques.org>
2021-03-03 00:43:16 +0700

    Add the role for game, it lost (game over) or win

--- myGame.py
+++ myGame.py
@@ -2,43 +2,45 @@ import tkinter as tk
 import random
 
 
+
 root = tk.Tk()
 root.resizable(width=False, height=False)
-root.geometry('1200x1000')
+root.geometry('1200x550')
 root.title('Pacman game')
 canvas = tk.Canvas(root)
 #VARIABLE
 #-----------------------------*CREATE GRID OF GAME*---------------------------------------
 countCoins = 0
-finished = True
+notFinished = True
+notWin = True
 choiceToMove = []
+indexOfPlayer = []
+
+canvas = tk.Canvas(root, bg='#455a64')
 
 grid = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 4, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 4, 1],
-    # [1, 2, 1, 1, 2, 1, 2, 1, 1, 0, 1, 1, 2, 1, 2, 1, 1, 2, 1],
-    # [1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1],
-    # [1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1],
-    # [1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1],
-    # [1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1],
-    # [1, 1, 1, 1, 2, 1, 2, 2, 2, 0, 2, 2, 4, 1, 2, 1, 1, 1, 1],
-    # [1, 1, 1, 1, 2, 1, 2, 0, 0, 0, 0, 0, 2, 1, 2, 1, 1, 1, 1],
-    # [1, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 1],
-    # [1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1],
-    # [1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-    # [1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1],
-    [1, 2, 2, 1, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 1, 2, 2, 1],
+    [1, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 1],
+    [1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1],
     [1, 2, 2, 4, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 4, 2, 2, 1],
     [1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1],
-    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] 
 ]
+
+#--------------*NUMBER 0 IS NOTHING*--------------------------#
+#--------------*NUMBER 1 IS WALLS*----------------------------#
+#--------------*NUMBER 2 IS COINS*----------------------------#
+#--------------*NUMBER 3 IS PLAYER*---------------------------#
+#--------------*NUMBER 4 IS ENEMIES*--------------------------#
+
 
 #-----------------------------*CREATE FUNCTION TO DRAW GIRD*---------------------------------------
 
 def  mySquare():
-    global myScore, enemy, finished
+    global myScore, enemy, pacMan
     myScore = 'SCORE: ' + str(countCoins)
     for row in range(len(grid)):
         for  col in  range(len(grid[row])):
@@ -57,18 +59,40 @@ def  mySquare():
                 pacMan = canvas.create_image(x1, y1, image=myPac, anchor='nw')
             elif grid[row][col] == 4:
                 enemy = canvas.create_image(x1, y1, image=theEnemy,anchor='nw')
-    canvas.create_text(250, 25, text=myScore, font=('consolas', 24, 'bold'), fill='green')
+    canvas.create_text(250, 25, text=myScore, font=('consolas', 24, 'bold'), fill='#e0e0e0')
 
 
-#-----------------------------*FUNCTION TO FIND INDEX OF 3*---------------------------------------
+#-----------------------------*FUNCTION TO FIND INDEX OF PLAYER*---------------------------------------
 
-def movePac(grid):
+def indexOfPac(grid):
+    global indexOfPlayer
     for index1 in range(len(grid)):
         for index2 in range(len(grid[index1])):
             if grid[index1][index2] == 3:
-                indexOfThree = [index1, index2]
-    return indexOfThree
+                indexOfPlayer = [index1, index2]
+    return indexOfPlayer
 
+
+#-----------------------------*END GAME WITH RESULT LOST (GAME OVER) AND WIN*---------------------------------------
+
+def clean():
+    global grid
+    grid = []
+    mySquare()
+
+def yourScore():
+    global countCoins, notFinished
+    if not notFinished:
+        canvas.delete('all')
+        if countCoins != 750:
+            canvas.create_image(0, 0, image=overGame, anchor='nw')
+            canvas.create_text(600,450, text="Your Score "+str(countCoins), font=(('couriernew'), 38, 'bold'), fill='#2196f3')
+        else:
+            canvas.create_image(0, 0, image=win, anchor='nw')
+            canvas.create_text(600, 470, text="YOU WIN", font=(('consolas'), 48, 'bold'), fill='#b71c1c')
+
+
+#-----------------------------*FUNCTION TO FIND INDEX OF ENEMY*---------------------------------------
 def indexOfEnemy(grid):
     indexEnemy = []
     for index1 in range(len(grid)):
@@ -77,107 +101,6 @@ def indexOfEnemy(grid):
                 indexEnemy.append([index1, index2])
     return indexEnemy
 
-
-
-#-----------------------------*FUNCTION TO MOVE PACMAN TO LEFT POSITION*---------------------------------------
-
-def moveLeft(event):
-    global grid, countCoins, finished
-    indexOfPlayer = movePac(grid)
-    rowOfThree = indexOfPlayer[0]
-    colOfThree = indexOfPlayer[1]
-
-    # MOVE LEFT POSITION OF PACMAN
-
-    if grid[rowOfThree][colOfThree-1] == 2:
-            countCoins += 10
-
-    if grid[rowOfThree][colOfThree-1] != 1 and grid[rowOfThree][colOfThree-1] != 4:
-        grid[rowOfThree][colOfThree] = 0
-        grid[rowOfThree][colOfThree-1] = 3
-    elif grid[rowOfThree][colOfThree-1] != 1 and grid[rowOfThree][colOfThree-1] == 4:
-        finished = False
-    
-    if finished:
-        canvas.delete('all')
-        mySquare()
-    else:
-        endGame()
-
-
-#-----------------------------*FUNTCTION TO MOVE PACMAN TO RIGHT POSITION*---------------------------------------
-
-def moveRight(event):
-    global grid, countCoins, finished
-    indexOfThree = movePac(grid)
-    rowOfThree = indexOfThree[0]
-    colOfThree = indexOfThree[1]
-    
-    # MOVE LEFT POSITION OF PACMAN
-    if grid[rowOfThree][colOfThree+1] == 2:
-        countCoins += 10
-
-    if grid[rowOfThree][colOfThree+1] != 1 and grid[rowOfThree][colOfThree+1] != 4:
-        grid[rowOfThree][colOfThree] = 0
-        grid[rowOfThree][colOfThree+1] = 3
-    elif grid[rowOfThree][colOfThree+1] != 1 and grid[rowOfThree][colOfThree+1] == 4:
-        finished = False
-
-    if finished:
-        canvas.delete('all')
-        mySquare()
-    else:
-        endGame()
-
-
-#-----------------------------*FUNCTION TO MOVE PACMAN TO UP POSITION*---------------------------------------
-
-def moveUp(event):
-    global grid, countCoins, finished
-    indexOfThree = movePac(grid)
-    rowOfThree = indexOfThree[0]
-    colOfThree = indexOfThree[1]
-
-
-    if grid[rowOfThree-1][colOfThree] == 2:
-        countCoins += 10
-    
-    if grid[rowOfThree-1][colOfThree] != 1:
-        grid[rowOfThree][colOfThree] = 0
-        grid[rowOfThree-1][colOfThree] = 3
-    elif grid[rowOfThree-1][colOfThree] != 1 and grid[rowOfThree-1][colOfThree] == 4:
-        finished = False
-
-    if finished:
-        canvas.delete('all')
-        mySquare()
-    else:
-        endGame()
-
-
-#-----------------------------*FUCNTION TO MOVE PACMAN TO DOWN POSITION*---------------------------------------
-
-
-def moveDown(event):
-    global grid, countCoins, finished
-    indexOfThree = movePac(grid)
-    rowOfThree = indexOfThree[0]
-    colOfThree = indexOfThree[1]
-
-    if grid[rowOfThree+1][colOfThree] == 2:
-        countCoins += 10
-
-    if grid[rowOfThree+1][colOfThree] != 1 :
-        grid[rowOfThree][colOfThree] = 0
-        grid[rowOfThree+1][colOfThree] = 3
-    elif grid[rowOfThree+1][colOfThree] != 1 and grid[rowOfThree+1][colOfThree] == 4:
-        finished = False
-
-    if finished:
-        canvas.delete('all')
-        mySquare()
-    else:
-        endGame()
 
 #-----------------------------*MOVE ENEMY*---------------------------------------
 
@@ -195,7 +118,7 @@ def canMove(grid, r, c):
     return choiceToMove
 
 def moveEnemy():
-    global grid, choiceToMove, finished
+    global grid, choiceToMove, notFinished
     enemiesIndex = indexOfEnemy(grid)
     for enemyIndex in enemiesIndex:   
         rIndex = enemyIndex[0]
@@ -205,89 +128,191 @@ def moveEnemy():
             toMove = random.choice(whereToMove)
             if toMove == 'u':
                 if grid[rIndex-1][cIndex] == 3:
-                    finished = False
+                    notFinished = False
                 elif grid[rIndex-1][cIndex] != 3 and grid[rIndex-1][cIndex] == 0:
                     grid[rIndex-1][cIndex] = 4
                     grid[rIndex][cIndex] = 0
                 elif grid[rIndex-1][cIndex] != 3 and grid[rIndex-1][cIndex] == 2:
                     grid[rIndex-1][cIndex] = 4
                     grid[rIndex][cIndex] = 2
-            elif toMove == 'r':
+            if toMove == 'r':
                 if grid[rIndex][cIndex+1] == 3:
-                    finished = False
+                    notFinished = False
                 elif grid[rIndex][cIndex+1] != 3 and grid[rIndex][cIndex+1] == 0:
                     grid[rIndex][cIndex+1] = 4
                     grid[rIndex][cIndex] = 0
                 elif grid[rIndex][cIndex+1] != 3 and grid[rIndex][cIndex+1] == 2:
                     grid[rIndex][cIndex+1] = 4
                     grid[rIndex][cIndex] = 2
-            elif toMove == 'd':
+            if toMove == 'd':
                 if grid[rIndex+1][cIndex] == 3:
-                    finished = False
-                elif grid[rIndex+1][cIndex] != 3 and grid[rIndex+1][cIndex] == 0:
+                    notFinished = False
+                elif grid[rIndex+1][cIndex] != 3 and grid[rIndex+1][cIndex] == 0 :
                     grid[rIndex+1][cIndex] = 4
                     grid[rIndex][cIndex] = 0
-                elif grid[rIndex+1][cIndex] != 3 and grid[rIndex+1][cIndex] == 2:
+                elif grid[rIndex+1][cIndex] != 3 and grid[rIndex+1][cIndex] == 2 :
                     grid[rIndex+1][cIndex] = 4
                     grid[rIndex][cIndex] = 2
-            elif toMove == 'l':
+            if toMove == 'l':
                 if grid[rIndex][cIndex-1] == 3:
-                    finished = False
-                elif grid[rIndex][cIndex-1] != 3 and grid[rIndex][cIndex-1] == 0:
+                    notFinished = False
+                elif grid[rIndex][cIndex-1] != 3 and grid[rIndex][cIndex-1] == 0 :
                     grid[rIndex][cIndex-1] = 4
                     grid[rIndex][cIndex] = 0
-                elif grid[rIndex][cIndex-1] != 3 and grid[rIndex][cIndex-1] == 2:
+                elif grid[rIndex][cIndex-1] != 3 and grid[rIndex][cIndex-1] == 2  :
                     grid[rIndex][cIndex-1] = 4
                     grid[rIndex][cIndex] = 2
-    if finished:
-        canvas.delete('all')
-        mySquare()
-        canvas.after(300, moveEnemy)
-    else:
-        endGame()
 
-#-----------------------------*END GAME WHEN THE GAME LOST*---------------------------------------
-
-
-def endGame():
-    canvas.after(1000, lambda:gameOver())
-    canvas.after(1500, lambda:yourScore())
-   
-
-
-def yourScore():
-    global countCoins
-    canvas.create_text(600, 680, text="Your Score "+str(countCoins), font=(('couriernew'), 38), fill='#e21a06')
-
-def gameOver():
     canvas.delete('all')
-    canvas.create_image(0, 0, image=overGame, anchor='nw')
+    mySquare()
+    canvas.after(400, moveEnemy)
+    if not notFinished:
+        clean()
+        yourScore()
 
-#-----------------------------*END GAME WHEN THE GAME WIN*---------------------------------------
 
-# def scoreToWin():
-#     global countCoins, end
-#     if countCoins == 670 and not end:
-#         canvas.delete('all')
-#         canvas.create_image(0, 0, image=win, anchor='nw')
-#         canvas.after(800, lambda:youWin())
-#     return None
-        
+#--------------*NUMBER 0 IS NOTHING*--------------------------#
+#--------------*NUMBER 1 IS WALLS*----------------------------#
+#--------------*NUMBER 2 IS COINS*----------------------------#
+#--------------*NUMBER 3 IS PLAYER*---------------------------#
+#--------------*NUMBER 4 IS ENEMIES*--------------------------#
 
-# def youWin():
-#     canvas.create_text(600, 850, text='YOU WIN', font=(('consolas'), 58), fill='#ec09c4')
-#     endGame()
+
+
+
+#-----------------------------*FUNCTION TO MOVE PACMAN TO LEFT POSITION*---------------------------------------
+
+def moveLeft(event):
+    global grid, countCoins, notFinished, notWin
+    numOfIndex = indexOfPac(grid)
+    rowNum = numOfIndex[0]
+    colNum = numOfIndex[1]
+
+    if notFinished:
+        if grid[rowNum][colNum-1] != 1:
+            grid[rowNum][colNum] = 0
+            if grid[rowNum][colNum-1] == 2:
+                countCoins += 10
+            if grid[rowNum][colNum-1] == 4:
+                notFinished = False
+                clean()
+            if notFinished:
+                grid[rowNum][colNum-1] = 3
+            if countCoins ==  750:
+                notFinished = False
+                clean()
+
+    canvas.delete('all')
+    mySquare()
+    yourScore()
+    
+
+
+
+#-----------------------------*FUNTCTION TO MOVE PACMAN TO RIGHT POSITION*---------------------------------------
+
+def moveRight(event):
+    global grid, countCoins, notFinished,notWin
+    numOfIndex = indexOfPac(grid)
+    rowNum = numOfIndex[0]
+    colNum = numOfIndex[1]
+    
+    # MOVE LEFT POSITION OF PACMAN
+
+    if notFinished:
+        if grid[rowNum][colNum+1] != 1:
+            grid[rowNum][colNum] = 0
+            if grid[rowNum][colNum+1] == 2:
+                countCoins += 10
+            if grid[rowNum][colNum+1] == 4:
+                notFinished = False
+                clean()
+            if notFinished:
+                grid[rowNum][colNum+1] = 3
+            if countCoins ==  750:
+                notFinished = False
+                clean()
+
+    canvas.delete('all')
+    mySquare()
+    yourScore()
+    
+
+
+#-----------------------------*FUNCTION TO MOVE PACMAN TO UP POSITION*---------------------------------------
+
+def moveUp(event):
+    global grid, countCoins, notFinished, notWin
+    numOfIndex = indexOfPac(grid)
+    rowNum = numOfIndex[0]
+    colNum = numOfIndex[1]
+
+    if notFinished:
+        if grid[rowNum-1][colNum] != 1:
+            grid[rowNum][colNum] = 0
+            if grid[rowNum-1][colNum] == 2:
+                countCoins += 10
+            if grid[rowNum-1][colNum] == 4:
+                notFinished = False
+                clean()
+            if notFinished:
+                grid[rowNum-1][colNum] = 3
+
+            if countCoins ==  750:
+                notFinished = False
+                clean()
+
+    canvas.delete('all')
+    mySquare()
+    yourScore()
+    
+    
+
+
+
+#-----------------------------*FUCNTION TO MOVE PACMAN TO DOWN POSITION*---------------------------------------
+
+
+def moveDown(event):
+    global grid, countCoins, notFinished
+    numOfIndex = indexOfPac(grid)
+    rowNum = numOfIndex[0]
+    colNum = numOfIndex[1]
+
+    if notFinished:
+        if grid[rowNum+1][colNum] != 1:
+            grid[rowNum][colNum] = 0
+            if grid[rowNum+1][colNum] == 2:
+                countCoins += 10
+            if grid[rowNum+1][colNum] == 4:
+                notFinished = False
+                clean()
+            if notFinished:
+                grid[rowNum+1][colNum] = 3
+
+            if countCoins ==  750:
+                notFinished = False
+                clean()
+
+    canvas.delete('all')
+    mySquare()
+    yourScore()
+
+
+
 
 
 
 #-----------------------------*PUT IMAGE TO SHOW ON INTERFACE*---------------------------------------
 
+
 wall = tk.PhotoImage(file='walls.png')
 myCoin = tk.PhotoImage(file='coin.png')
-myPac = tk.PhotoImage(file='pacman.png')
+myPac = tk.PhotoImage(file='male.png')
 theEnemy = tk.PhotoImage(file='enemy.png')
 overGame = tk.PhotoImage(file='over.png')
 win = tk.PhotoImage(file='youWin.png')
+
 
 #-----------------------------*USE KEY LEFT, RIGHT, UP, DOWN TO MOVE PACMAN*---------------------------------------
 
@@ -300,11 +325,14 @@ root.bind("<Down>",moveDown) # MOVE PLAYER TO DOWN
 
 
 
-#-----------------------------*SHOW RESULT ON SCREEN*---------------------------------------
+canvas.pack(expand=True, fill='both')
+
+
+#-----------------------------*CALL FUNCTION*---------------------------------------
 
 mySquare()
 moveEnemy()
 
 
-canvas.pack(expand=True, fill='both')
+#-----------------------------*DISPLAY WINDOWS*---------------------------------------
 root.mainloop()
